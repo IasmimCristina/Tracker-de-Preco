ceee2e168b22fa8fa24ae96d5f036a440b02fd21
IasmimCristina <iaasmimcristinaa@gmail.com>
2023-10-26 12:49:27 -0300

    added assets and initiated navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 const spaceGrotesk = Space_Grotesk({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] })
 export const metadata: Metadata = {
   title: 'PreçoSábio',
-  description: 'Acompanhe preços da melhor forma possível e gaste menos dinheiro',
+  description: 'Acompanhe preços da melhor forma possível e gaste menos dinheiro!',
 }
 
 export default function RootLayout({
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        {/* Always importante to set a max-width */}
         <main className="max-w-10xl mx-auto">
           <Navbar />
           {children}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='w-full'>
+
+      <nav className='nav'>
+        <Link href={"/"} className='flex items-center gap-1'>
+
+<Image  alt='Logomarca' width={27} height={27} src={"/assets/icons/logo.svg"}/>
+
+<p className='nav-logo'></p>
+        </Link>
+      </nav>
+    </header>
   )
 }
 
-export default Navbar
+export default Navbar;
