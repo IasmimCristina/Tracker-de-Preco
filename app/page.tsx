3dc703681ecdc758a44f5bdd0865a98d70a91012
IasmimCristina <iaasmimcristinaa@gmail.com>
2023-10-26 16:33:00 -0300

    visual for search bar done

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,60 @@
+import HeroCarousel from '@/components/HeroCarousel'
+import Searchbar from '@/components/Searchbar'
+import Image from 'next/image'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+  <> 
+  
+  <section className="px-6 border-2 md:px-20 py-24 ">
+
+<div className="flex max-xl:flex-col gap-16">
+
+  <div className="flex flex-col justify-center">
+
+    <p className="small-text">
+      Comprar de maneira inteligente começa aqui:
+      <Image
+      src="/assets/icons/arrow-right.svg"
+      alt='seta para direita'
+      width={16}
+      height={16}
+      
+      />
+    </p>
+
+    <h1 className='head-text'>
+      Liberte o poder do <br/>
+      <span className="text-red-400">PreçoSábio</span>
+    </h1>
+
+    <p className="mt-6">
+    Acompanhe preços de produtos e encontre as melhores ofertas. Compre conscientemente e economize enquanto faz compras!
+    </p>
+
+    <Searchbar/>
+  </div>
+
+
+    <HeroCarousel/>
+
+
+</div>
+
+  </section>
+
+
+  <div className="trending-section">
+    <h2 className="section-text">Trending!</h2>
+
+    <div className="flex flex-wrap gap-x-8 gap-y-16">
+      {['Apple Iphone 15', 'Book', 'Sneakers'].map((product) => (
+        <div className="">{product}</div>
+      ))}
+    </div>
+  </div>
+  </>
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,17 +2,34 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
+const navIcons = [
+  { src: 'assets/icons/search.svg', alt: 'Busca' },
+  { src: 'assets/icons/black-heart.svg', alt: 'Coração' },
+  { src: 'assets/icons/user.svg', alt: 'Usuário' },
+
+]
+
 const Navbar = () => {
   return (
-    <header className='w-full'>
+    <header className='w-full bg-slate-300'>
 
-      <nav className='nav'>
+      <nav className='nav '>
         <Link href={"/"} className='flex items-center gap-1'>
 
-<Image  alt='Logomarca' width={27} height={27} src={"/assets/icons/logo.svg"}/>
+          <Image title='PreçoSábio' alt='Logomarca' width={27} height={27} src={"/assets/icons/logo.svg"} />
 
-<p className='nav-logo'></p>
+          <p className='nav-logo'>Preço<span className='text-primary'>Sábio</span></p>
         </Link>
+
+        <div className="flex item-center gap-5">
+          {navIcons.map((icon) => (
+
+            <img src={icon.src} alt={icon.alt} width={30} height={30} className='object-contain  cursor-pointer ease-out duration-300 hover:bg-red-500 rounded-full text-center p-1' />
+
+
+          ))}
+
+        </div>
       </nav>
     </header>
   )
